0df26f06922e149f57868b0c7ed7fb5ab96d32ea
Aung Ko Ko <AungWorld@live.com>
2026-04-09 12:18:22 +0630

    Add Xamarin.Android direct SDK approach - replaces failed MAUI builds

---
+++ AkkDictionary.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+namespace AkkDictionary.Android;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        SetContentView(Resource.Layout.activity_main);
+
+        // Find UI elements
+        var searchInput = FindViewById<EditText>(Resource.Id.searchInput);
+        var searchButton = FindViewById<Button>(Resource.Id.searchButton);
+        var resultsView = FindViewById<ListView>(Resource.Id.resultsView);
+
+        // Wire up search button click
+        searchButton.Click += (s, e) => {
+            string query = searchInput.Text ?? "";
+            // TODO: Implement search logic with database
+            Toast.MakeText(this, "Search for: " + query, ToastLength.Short)?.Show();
+        };
+    }
+}
